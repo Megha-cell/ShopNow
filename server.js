f0876b45a3e6ef67caff09d3c3d2aae37e21878d
Megha-cell <meghakamlekar123@gmail.com>
2025-05-03 22:24:47 +0530

    Few changes on Package.json

--- server.js
+++ server.js
@@ -24,10 +24,7 @@ app.use("/api/users", userRoutes);
 
 //Connect to mongodb
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB Connection Error:", err));
 
