77be0348ec2d7099302d8591ae1c07e06acca125
Megha-cell <meghakamlekar123@gmail.com>
2025-05-04 21:25:09 +0530

    server.js changes

--- server.js
+++ server.js
@@ -60,14 +60,12 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB Connection Error:", err));
-
+// Test route for API check
+app.get("/", (req, res) => res.send("API is running..."));
 // Serve React app for all other routes
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
-
-// Test route for API check
-app.get("/", (req, res) => res.send("API is running..."));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
